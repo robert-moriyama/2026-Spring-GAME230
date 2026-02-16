f5cc79afe8cbace3877d28ff7bd607eeef18ce32
Robert Moriyama <robert.moriyama@outlook.com>
2026-02-16 11:58:25 -0800

    Declared and initialized variables with user input

--- homework/february_19/project/Homework_Feb19/Program.cs
+++ homework/february_19/project/Homework_Feb19/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string stringA, stringB; // Variables for the two values the user inputs (string type)
+double doubleA, doubleB; // Variables for the two values the user inputs (converted into double type)
+string operation; // Variable for the operation the user inputs (string type)
 
-Console.WriteLine("Hello, World!");
+// First number (A)
+Console.WriteLine("What is your first number?");
+stringA = Console.ReadLine();
+doubleA = double.Parse(stringA);
+
+// Second number (B)
+Console.WriteLine("What is your second number?");
+stringB = Console.ReadLine();
+doubleB = double.Parse(stringB);
+
+// Operation
+Console.WriteLine("What operation do you want to perform? Type 'addition', 'subtraction', 'multiplication', or 'division'.");
+operation = Console.ReadLine();
