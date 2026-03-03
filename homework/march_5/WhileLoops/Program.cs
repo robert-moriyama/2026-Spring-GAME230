e63943e3a232074b004acb4cfa8406129f593ac9
Robert Moriyama <robert.moriyama@outlook.com>
2026-03-02 20:09:08 -0800

    Completed questions 1-6 and 8-9

--- homework/march_5/WhileLoops/Program.cs
+++ homework/march_5/WhileLoops/Program.cs
@@ -1,1 +1,114 @@
-﻿
+﻿// Question 1
+Console.WriteLine("Question 1:");
+int index1 = 1;
+
+while (index1 <= 5)
+{
+    Console.Write(index1 + " ");
+    index1++;
+}
+
+
+// Question 2
+Console.WriteLine("\n\nQuestion 2:");
+int index2 = 100;
+
+while (index2 <= 150)
+{
+    Console.Write(index2 + " ");
+    index2++;
+}
+
+
+// Question 3
+Console.WriteLine("\n\nQuestion 3:");
+int index3 = 0;
+
+while (index3 <= 100)
+{
+    Console.Write(index3 + " ");
+    index3 += 2;
+}
+
+
+// Question 4
+Console.WriteLine("\n\nQuestion 4:");
+int index4 = 20;
+
+while (index4 >= -20)
+{
+    Console.Write(index4 + " ");
+    index4--;
+}
+
+
+// Question 5
+Console.WriteLine("\n\nQuestion 5:");
+int index5 = 1;
+
+while (index5 <= 100)
+{
+    Console.Write(index5 + " ");
+    index5 += 3;
+}
+
+
+// Question 6
+Console.WriteLine("\n\nQuestion 6:");
+int index6 = 1;
+
+while (index6 <= 1024)
+{
+    Console.Write(index6 + " ");
+    index6 *= 2;
+}
+
+
+// Question 7
+Console.WriteLine("\n\nQuestion 7:");
+
+
+// Question 8
+Console.WriteLine("\n\nQuestion 8:");
+int index8 = 0;
+
+while (index8 < 100)
+{
+    if (index8 % 2 == 0)
+    {
+        Console.Write("True ");
+    }
+    else
+    {
+        Console.Write("False ");
+    }
+
+    index8++;
+}
+
+
+// Question 9
+Console.WriteLine("\n\nQuestion 9:");
+int index9 = 1;
+
+while (index9 <= 20)
+{
+    if (index9 % 2 == 0)
+    {
+        Console.Write(index9 + " is even ");
+    }
+    else
+    {
+        Console.Write(index9 + " is odd ");
+    }
+
+    index9++;
+}
+
+
+// Question 10
+Console.WriteLine("\n\nQuestion 10:");
+
+
+// Bonus
+Console.WriteLine("\n\nBonus:");
